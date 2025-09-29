072d69cbac7be6e6a481137d787eee92f471ef87
Super-nova511 <surekhakamble2112@gmail.com>
2025-09-29 20:39:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-from flask import Flask, send_file
 import os
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route("/health")
-def home():
-    return "Working"
-
 @app.route("/")
+def home():
+    return '<h2>Welcome! Go to /download to download the ZIP file.</h2>'
+
+@app.route("/download")
 def download():
-    path = "messs5.zip"  # Your ZIP file
-    return send_file(path, as_attachment=True)
+    return send_file("messs5.zip", as_attachment=True)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-
     app.run(host="0.0.0.0", port=port)
